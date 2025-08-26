41b3dbb06f361a4655ac8db776b6ec140058dc61
Nuzli Hernawan <nuzlilatief@gmail.com>
2025-08-26 15:25:07 +0700

    modified:   netlify.toml modified:   netlify/functions/server.js

--- netlify/functions/server.js
+++ netlify/functions/server.js
@@ -1,1 +1,5 @@
-const serverless = require('serverless-http
+const serverless = require('serverless-http');
+const { server } = require('../../server/index.js');
+
+// Export the server as a Netlify function
+module.exports.handler = serverless(server);
