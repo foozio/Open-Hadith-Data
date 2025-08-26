ce81f66f79e95e1738106816c9a63d7f65105af1
Nuzli Hernawan <nuzlilatief@gmail.com>
2025-08-26 16:26:17 +0700

    modified:   netlify.toml modified:   netlify/functions/server.js

--- netlify/functions/server.js
+++ netlify/functions/server.js
@@ -2,4 +2,13 @@ const serverless = require('serverless-http');
 const { server } = require('../../server/index.js');
 
 // Export the server as a Netlify function
-module.exports.handler = serverless(server);
+// Make sure to handle binary media types correctly
+module.exports.handler = serverless(server, {
+  binary: ['*/*'],
+  request: function(request, event, context) {
+    // Ensure the request path is correctly forwarded
+    request.path = event.path;
+    request.method = event.httpMethod;
+    return request;
+  }
+});
